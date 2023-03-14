84c98598e4cfb8cfc09b58b63caaa2a6fd27ca00
guyu <guyu@bixin.cn>
2023-03-15 00:01:59 +0800

    init

---
+++ rain-dubbo-api/src/main/java/com/good/day/api/AnswerService.java
@@ -0,0 +1,10 @@
+package com.good.day.api;
+
+/**
+ * @author guyu
+ * @desc:
+ * @date 2023/3/14 23:37
+ */
+public interface AnswerService {
+    public void reportSafe();
+}
---
+++ rain-dubbo-api/src/main/java/com/good/day/req/ReportSafeReq.java
@@ -0,0 +1,14 @@
+package com.good.day.req;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author guyu
+ * @desc:
+ * @date 2023/3/14 23:38
+ */
+@Data
+public class ReportSafeReq implements Serializable {
+}
